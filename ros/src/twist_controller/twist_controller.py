7943ca671aadd8816f949880b3a32e30e3d52629
freddyman2k <fryderyk@mantiuk.de>
2020-11-16 11:26:19 -0800

    First commit

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -1,14 +1,64 @@
+from pid import PID
+from lowpass import LowPassFilter
+from yaw_controller import YawController
+import rospy
 
 GAS_DENSITY = 2.858
 ONE_MPH = 0.44704
 
 
 class Controller(object):
-    def __init__(self, *args, **kwargs):
-        # TODO: Implement
-        pass
+    def __init__(self, vehicle_mass, fuel_capacity, brake_deadband, decel_limit, accel_limit,
+                 wheel_radius, wheel_base, steer_ratio, max_lat_accel, max_steer_angle):
+        
+        self.yaw_controller = YawController(wheel_base=wheel_base, steer_ratio=steer_ratio, 
+                                            min_speed=0.1, max_lat_accel=max_lat_accel, 
+                                            max_steer_angle=max_steer_angle)
+        
+        min_throttle = 0
+        max_throttle = 0.2
+        self.throttle_controller = PID(kp=0.3, ki=0.1, kd=0, mn=min_throttle, mx=max_throttle)
 
-    def control(self, *args, **kwargs):
-        # TODO: Change the arg, kwarg list to suit your needs
-        # Return throttle, brake, steer
-        return 1., 0., 0.
+        tau = 0.5 # 1/(2pi*tau) = cutoff frequency
+        ts = .02 # sample time
+        self.vel_lowpass = LowPassFilter(tau, ts)
+
+        self.vehicle_mass = vehicle_mass
+        self.decel_limit = decel_limit
+        self.wheel_radius = wheel_radius
+
+        self.last_time = rospy.get_time()
+
+    def control(self, current_vel, dbw_enabled, linear_vel, angular_vel):
+        if not dbw_enabled: 
+            # Drive-By-Wire is disabled in the car => Manual Drive
+            # Reset the PID Controller to prevent the error from falsely accumulating
+            self.throttle_controller.reset()
+            return 0, 0, 0
+
+        current_vel = self.vel_lowpass.filt(current_vel)
+
+        steer = self.yaw_controller.get_steering(linear_vel, angular_vel, current_vel)
+
+        vel_error = linear_vel - current_vel
+
+        current_time = rospy.get_time()
+        sample_time = current_time - self.last_time
+        self.last_time = current_time
+
+        throttle = self.throttle_controller.step(vel_error, sample_time)
+        brake = 0
+
+        # Cases where the car should brake:
+        if linear_vel == 0 and current_vel < 0.1:
+            # hold the car in place if we are stopped at a light
+            throttle = 0
+            brake = 700 # N*m, torque to prevent Carla from moving
+        elif throttle < 0.1 and vel_error < 0:
+            # car is going faster than the target velocity
+            # and PID has already decreased the throttle
+            # => brake
+            decel = max(vel_error, self.decel_limit)
+            brake = abs(decel) * self.vehicle_mass * self.wheel_radius # Torque N*m
+
+        return throttle, brake, steer
--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 
+import math
+
+from scipy.spatial import KDTree
+import numpy as np
+
 import rospy
 from geometry_msgs.msg import PoseStamped
 from styx_msgs.msg import Lane, Waypoint
-
-import math
 
 '''
 This node will publish waypoints from the car's current position to some `x` distance ahead.
@@ -36,17 +39,60 @@ class WaypointUpdater(object):
 
         self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
 
-        # TODO: Add other member variables you need below
+        self.pose = None
+        self.base_waypoints = None
+        self.waypoints_2d = None 
+        self.waypoint_tree = None
 
-        rospy.spin()
+        self.loop()
+
+        
+    def loop(self):
+        rate = rospy.Rate(50)
+        while not rospy.is_shutdown():
+            if self.pose and self.base_waypoints:
+                # Get closest waypoint
+                closest_waypoint_idx = self.get_closest_waypoint_idx()
+                self.publish_waypoints(closest_waypoint_idx)
+            rate.sleep()
+
+    def get_closest_waypoint_idx(self):
+        x = self.pose.pose.position.x
+        y = self.pose.pose.position.y
+        closest_idx = self.waypoint_tree.query([x, y], 1)[1]
+
+        # Check if closest is ahead or behind vehicle
+        closest_coord = self.waypoints_2d[closest_idx]
+        prev_coord = self.waypoints_2d[closest_idx-1]
+
+        # Arrays for dot product
+        cl_vect = np.array(closest_coord)
+        prev_vect = np.array(closest_coord)
+        pos_vect = np.array([x, y])
+
+        val = np.dot(cl_vect-prev_vect, pos_vect-cl_vect)
+
+        if val > 0:
+            # dot product is positive
+            # => closest waypoint is behind the vehicle, get next wp
+            closest_idx = (closest_idx + 1) % len(self.waypoints_2d)
+        return closest_idx
+
+
+    def publish_waypoints(self, closest_idx):
+        lane = Lane()
+        lane.header = self.base_waypoints.header
+        lane.waypoints = self.base_waypoints.waypoints[closest_idx : closest_idx + LOOKAHEAD_WPS]
+        self.final_waypoints_pub.publish(lane)
 
     def pose_cb(self, msg):
-        # TODO: Implement
-        pass
+        self.pose = msg
 
     def waypoints_cb(self, waypoints):
-        # TODO: Implement
-        pass
+        self.base_waypoints = waypoints
+        if not self.waypoints_2d:
+            self.waypoints_2d = [[waypoint.pose.pose.position.x, waypoint.pose.pose.position.y] for waypoint in waypoints.waypoints]
+            self.waypoint_tree = KDTree(self.waypoints_2d)  
 
     def traffic_cb(self, msg):
         # TODO: Callback for /traffic_waypoint message. Implement
@@ -63,6 +109,10 @@ class WaypointUpdater(object):
         waypoints[waypoint].twist.twist.linear.x = velocity
 
     def distance(self, waypoints, wp1, wp2):
+        """ 
+        Calculates the distance from waypoint at wp1 to the waypoint at wp2 by 
+        accumulating the euclidean distances to each waypoint inbetween wp1 and wp2.
+        """
         dist = 0
         dl = lambda a, b: math.sqrt((a.x-b.x)**2 + (a.y-b.y)**2  + (a.z-b.z)**2)
         for i in range(wp1, wp2+1):
